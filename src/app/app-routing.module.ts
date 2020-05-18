8a4454bc9deb4fea4685bcd9651d43c7288b8304
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-05-17 22:35:01 -0400

    basic initial set up stuffs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DungeonComponent } from './pages/dungeon/dungeon.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '/dungeon', component: DungeonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
