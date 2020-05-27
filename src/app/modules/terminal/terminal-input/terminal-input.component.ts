31c81c59834b38d3d671ffc8a039507ee362bd02
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-05-27 16:31:59 -0400

    added up / down arrow functionality in terminal

--- src/app/modules/terminal/terminal-input/terminal-input.component.ts
+++ src/app/modules/terminal/terminal-input/terminal-input.component.ts
@@ -7,10 +7,33 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class TerminalInputComponent {
   @Output() issueCommand = new EventEmitter<string>();
+  commandHistory: string[] = [];
+  currentCommand = 0;
   command = '';
 
   onSubmit(input: string): void {
+    this.commandHistory.unshift(input);
     this.issueCommand.emit(input);
     this.command = '';
+    this.currentCommand = -1;
+
+    if (this.commandHistory.length > 10) { this.commandHistory.pop(); }
+  }
+
+  getPreviousCommand(): void {
+    if (this.currentCommand >= this.commandHistory.length) { return; }
+
+    this.currentCommand++;
+    this.command = this.commandHistory[this.currentCommand];
+  }
+
+  getNextCommand(): void {
+    if (this.currentCommand < 0) {
+      this.command = '';
+      return;
+    }
+
+    this.currentCommand--;
+    this.command = this.commandHistory[this.currentCommand];
   }
 }
